dd6bb943b492b9a1743b2c4a869a6cf6c5659534
dahalprastut <dahalprastut@gmail.com>
2024-03-09 21:52:59 -0500

    complex pointers with functions and void defination

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <cmath>
 
+void pointerFnc(char *a) {
+    std::cout << "addres of a is: " << a << "and its value is: " << *a << std::endl;
+};
+
+char changePointerVal (char *a) {
+    *a = 'd';
+    return *a;
+};
+
+
+void voidPointerFn(void* variab, char c) {
+    if(c == 'i') {
+        std::cout << "the address of variable is: " << ((int*)variab) << "and the value is " << *((int*)variab) << std::endl;
+    } else if(c == 'c') {
+        std::cout << "the address of variable is: " << ((char*)variab) << "and the value is " << *((char*)variab) << std::endl;
+    }
+};
 
 
 int main() {
    
 
-    // Pointers 
-    // Simple pointers
+    //    complex pointers
 
-    // Pointers are ways to reference to the variable in memory.
-
-    int a = 50;
-    int* ptr = &a;
-    std::cout << "Address of a is :" << ptr << "and the value of ptr is: " << *ptr <<  std::endl;
-    *ptr = 52;
-    std::cout << "Address of a is :" << ptr << "and the value of ptr is: " << *ptr << std::endl;
-    
-
+    // change the pointers using function
+    // int pointers cannot be used for string variables and vice versal.
+    char a = 12;
+    // pointerFnc(&a);
+    // a = changePointerVal(&a);
+    // std::cout << "addres of a is: " << &a << "and its value is: " << a << std::endl; 
+    voidPointerFn(&a, 'i');
 
 
 }
